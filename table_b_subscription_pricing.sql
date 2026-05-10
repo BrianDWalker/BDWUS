9bb36c0d95603aaa7a8e0530e250939e8ae6dc12
BrianDWalker <116129961+BrianDWalker@users.noreply.github.com>
2026-05-09 21:49:52 -0500

    Update table_b_subscription_pricing.sql

--- table_b_subscription_pricing.sql
+++ table_b_subscription_pricing.sql
@@ -1,20 +1,39 @@
 SELECT
+
     c.CustomerId AS customer_id,
+
     c.CustomerNumber AS customer_number,
+
     s.SubscriptionId AS subscription_id,
+
     rp.RatePlanId AS plan_id,
+
     rp.PlanName AS plan_name,
+
     rp.PlanTier AS plan_tier,
+
     svc.ServiceName AS service_name,
+
     svc.ServiceCategory AS service_category,
+
     s.Quantity AS quantity,
+
     rp.StandardRate AS standard_rate,
+
     s.CurrentRate AS current_rate,
+
     SYSUTCDATETIME() AS load_date_time
-FROM billing.Customer c
-INNER JOIN billing.Subscription s
+
+FROM billing.Customers c
+
+INNER JOIN billing.Subscriptions s
+
     ON c.CustomerId = s.CustomerId
-INNER JOIN billing.RatePlan rp
+
+INNER JOIN billing.RatePlans rp
+
     ON s.RatePlanId = rp.RatePlanId
-INNER JOIN billing.Service svc
+
+INNER JOIN billing.Services svc
+
     ON rp.ServiceId = svc.ServiceId;
