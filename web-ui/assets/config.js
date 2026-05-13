0c73de1a4820ae5a76e05ddc3da6c3bb423e82d4
Brian Walker <brianwalker@Brians-MacBook-Air.local>
2026-05-12 22:30:15 -0500

    Restore clean React web UI

--- web-ui/assets/config.js
+++
@@ -1,7 +0,0 @@
-window.APP_CONFIG = {
-  portalName: "Northstar Telecom",
-  user: {
-    displayName: "Ava Chen",
-    role: "Operations Manager"
-  }
-};
--- web-ui/src/main.jsx
+++ web-ui/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "../assets/styles.css";
+import "./base.css";
 import "./react-app.css";
-import PlatformApp from "./PlatformApp";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PlatformApp />
+    <App />
   </React.StrictMode>
 );
