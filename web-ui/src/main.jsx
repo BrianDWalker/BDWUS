b75b1909a96013830cf2bac43fc94e5f6bddb5fc
BrianDWalker <116129961+BrianDWalker@users.noreply.github.com>
2026-07-04 22:04:25 -0500

    Import quote to order action styles

--- web-ui/src/main.jsx
+++ web-ui/src/main.jsx
@@ -5,6 +5,7 @@ import "./react-app.css";
 import "./crm-polish.css";
 import "./sales-crm.css";
 import "./ui-consistency.css";
+import "./quote-to-order.css";
 import SalesAppRouter from "./SalesAppRouter";
 
 createRoot(document.getElementById("root")).render(
